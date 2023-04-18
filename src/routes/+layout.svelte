1a03cb4b93d4f2e5ca011a9ccf533a89ff2eaccc
jaasonw <jayywong92@gmail.com>
2023-04-17 17:52:57 -0700

    basic layout stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,22 @@
   <title>title</title>
 </svelte:head>
 
-<slot />
+<div class="drawer drawer-mobile">
+  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col items-center justify-center">
+    <!-- Page content here -->
+    <slot />
+    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden"
+      >Open drawer</label
+    >
+  </div>
+  <div class="drawer-side border border-red-400">
+    <label for="my-drawer-2" class="drawer-overlay" />
+    <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+      <!-- Sidebar content here -->
+      <li><a href="/explain">Explain Code</a></li>
+      <li><a href="/explain">Time Complexity</a></li>
+      <li><a href="/explain">Translate Code</a></li>
+    </ul>
+  </div>
+</div>
---
+++ src/routes/explain/+page.svelte
@@ -0,0 +1,25 @@
+<form
+  class="flex flex-col border h-5/6 w-4/5 p-2 gap-2 items-center md:h-80 max-w-3xl"
+>
+  <div class="flex flex-col w-full h-full md:flex-row">
+    <div class="flex flex-col w-full h-full md:w-1/2">
+      <span class="label-text">Code</span>
+      <select class="select select-bordered w-full">
+        <option disabled selected>Select Programming Language</option>
+        <option>C++</option>
+        <option>Python</option>
+        <option>Javascript</option>
+        <option>Typescript</option>
+      </select>
+      <textarea class="border resize-none w-full h-full p-2" />
+    </div>
+    <div class="flex flex-col w-full h-full md:w-1/2">
+      <span class="label-text">Explanation</span>
+      <select class="select select-bordered w-full">
+        <option selected>English</option>
+      </select>
+      <textarea class="border resize-none w-full h-full p-2" />
+    </div>
+  </div>
+  <button class="btn btn-wide">Submit</button>
+</form>
