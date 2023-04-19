1c6a46c33efe1a6e31e07a789e3beed358c89c77
jaasonw <jayywong92@gmail.com>
2023-04-19 16:10:27 -0700

    fix routes 3

--- src/routes/time_complexity/+page.svelte
+++ src/routes/time_complexity/+page.svelte
@@ -13,7 +13,7 @@
       data[key] = value;
     }
 
-    const rawResponse = fetch("./", {
+    const rawResponse = fetch("/", {
       method: "POST",
       headers: {
         Accept: "application/json",
