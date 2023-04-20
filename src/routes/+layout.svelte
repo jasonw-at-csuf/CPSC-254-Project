564d3379960816fa1118be3b0967737286e97d39
jaasonw <jayywong92@gmail.com>
2023-04-19 23:35:29 -0700

    mobile responsive smile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
   import "../app.css";
+  import {
+    ToiletsPortableSolid,
+    StarHalfRegular,
+    BarsSolid,
+  } from "svelte-awesome-icons";
 </script>
 
 <svelte:head>
@@ -8,20 +13,41 @@
 
 <div class="drawer drawer-mobile">
   <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col items-center justify-center">
-    <!-- Page content here -->
-    <slot />
-    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden"
-      >Open drawer</label
-    >
+  <div class="drawer-content">
+    <div class="flex flex-col w-full h-full justify-between items-end">
+      <nav
+        class="flex justify-end bg-base-100 text-base-content w-full border shadow-md p-4 lg:hidden"
+      >
+        <label for="my-drawer-2" class="drawer-button hover:cursor-pointer"
+          ><BarsSolid /></label
+        >
+      </nav>
+      <!-- <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden"
+        >Open drawer</label
+      > -->
+      <div class="flex flex-col h-full w-full items-center justify-center">
+        <slot />
+      </div>
+      <div class="flex flex-col m-4 text-right text-xs text-gray-400">
+        <span>open source under <span class="underline">MIT license</span></span
+        >
+        <a
+          class="hover:underline"
+          href="https://github.com/jasonw-at-csuf/CPSC-254-Project"
+          >view source on github</a
+        >
+      </div>
+    </div>
   </div>
   <div class="drawer-side border shadow-md">
     <label for="my-drawer-2" class="drawer-overlay" />
-    <ul class="menu p-4 w-80 bg-base-100 text-base-content">
-      <!-- Sidebar content here -->
-      <li><a href="./explain">Explain Code</a></li>
-      <li><a href="./time_complexity">Time Complexity</a></li>
-      <li><a href="./translate">Translate Code</a></li>
-    </ul>
+    <div class="menu p-4 w-80 bg-base-100 text-base-content gap-20">
+      <div class="p-4 font-bold">twofivefour</div>
+      <ul>
+        <li><a href="./explain">Explain Code</a></li>
+        <li><a href="./time_complexity">Time Complexity</a></li>
+        <li><a href="./translate">Translate Code</a></li>
+      </ul>
+    </div>
   </div>
 </div>
